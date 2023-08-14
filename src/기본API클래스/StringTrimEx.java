f35185b781f6e7f9f38a2f261118ac147fa15eaf
ktgg1868 <teaguyn1868@naver.com>
2023-08-14 20:34:49 +0900

    08/14 예제문제

---
+++ src/기본API클래스/StringTrimEx.java
@@ -0,0 +1,14 @@
+package 기본API클래스;
+
+public class StringTrimEx {
+    public static void main(String[] args) {
+        
+        String tel1 = "  02";
+        String tel2 = "123   ";
+        String tel3 = "   1234   ";
+
+        String tel = tel1.trim() + tel2.trim() + tel3.trim(); //공백잘라내기
+        System.out.println(tel);
+
+    }
+}
